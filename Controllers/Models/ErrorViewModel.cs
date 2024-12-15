bf5ca1d91811d6c941b5bc6656bfa6fc4b922556
cathysadykh <81162530+cathysadykh@users.noreply.github.com>
2024-12-15 18:47:17 +0000

    Initial commit

---
+++ Controllers/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EMGAutomobiles.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
